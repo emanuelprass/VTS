bff0f3a74c0df98e282a8c641379bc677a47ad84
BSIDOMAIN\BSI90400 <muflih.majid@gmail.com>
2022-03-09 12:39:38 +0700

    Update skeleton

---
+++ Application/UseCases/RegisterUser/RegisterUserCommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using SceletonAPI.Application.Interfaces;
+using SceletonAPI.Application.Models.Query;
+using SceletonAPI.Infrastructure.Persistences;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SceletonAPI.Domain.Entities;
+
+namespace SceletonAPI.Application.UseCases.RegisterUser {
+    public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand, RegisterUserDto> {
+        private readonly IDBContext _context;
+        private readonly IMapper _mapper;
+        private readonly IMediator _mediator;
+
+        public RegisterUserCommandHandler (IDBContext context, IMapper mapper, IMediator mediator) {
+            _context = context;
+            _mapper = mapper;
+            _mediator = mediator;
+        }
+
+        public async Task<RegisterUserDto> Handle (RegisterUserCommand request, CancellationToken cancellationToken) {
+            var user = new User ();
+            var response = new RegisterUserDto ();
+            // object tiket
+            user = new User () {
+                LastUpdateDate = DateTime.Now,
+                Email = request.Data.email,
+                Phone = request.Data.phone,
+                Name = request.Data.name,
+                Company = request.Data.company,
+                CreateDate = DateTime.Now,
+                ExpiredDate = DateTime.Now.AddHours (1),
+                Key = "Tettttttttttttt",
+                Verivy = false,
+                CreateBy = "Admin"
+            };
+            _context.Users.Add (user);
+            await _context.SaveChangesAsync (cancellationToken);
+            response.Success = true;
+            response.Message = "Register Success";
+            await _context.SaveChangesAsync (cancellationToken);
+            return response;
+        }
+    }
+
+}
